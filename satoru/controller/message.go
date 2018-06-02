1323fc571a5550f1100ac360a6b35d0abbffdd4c
Satorino01 <satorino0821@gmail.com>
2018-06-02 15:06:07 +0900

    UserNameのNが小文字だったので修正

--- satoru/controller/message.go
+++ satoru/controller/message.go
@@ -75,8 +75,8 @@ func (m *Message) Create(c *gin.Context) {
 
 	// Tutorial 1-2. ユーザー名を追加しよう
 	// できる人は、ユーザー名が空だったら`anonymous`等適当なユーザー名で投稿するようにしてみよう
-	if msg.Username == "" {
-		msg.Username = "ななしさん"
+	if msg.UserName == "" {
+		msg.UserName = "ななしさん"
 	}
 
 	inserted, err := msg.Insert(m.DB)
