f5154a9815cd202db6306dee8a3532bd54ba8910
toshusai <Shu@saitouhiiragis-MacBook-Pro.local>
2018-06-02 17:46:58 +0900

    Add rooms page.

---
+++ tockn/assets/js/rooms.js
@@ -0,0 +1,98 @@
+(function () {
+  'use strict';
+  const Message = function () {
+    this.body = ''
+    this.username = ''
+  };
+
+  Vue.component('room', {
+    props: {
+      data: {
+        id: "",
+        name: "",
+        image_url: "",
+        bitrh: "",
+      }
+    },
+    data() {
+      return {
+        editing: false,
+        editedBody: null,
+      }
+    },
+
+    template: `
+    <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" v-bind:src="data.image_url" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ data.name }}</h5>
+                        <p class="card-text">{{ data.birth }}</p>
+                        <a v-bind:href="'./rooms/' + data.id" class="btn btn-primary">部屋に入る</a>
+                    </div>
+                </div>
+            </div>
+  `,
+  mounted(){
+    console.log(this.data);
+  },
+    methods: {
+      remove() {
+        this.removeMessage(this.id)
+      }
+    }
+  });
+
+  const app = new Vue({
+    el: '#app',
+    data: {
+      rooms: [
+        {
+          id: "3",
+          name: "namae",
+          image_url: "https://nekogazou.com/wp-content/uploads/2015/03/282e6ed4976b181c78381a609c0f4e32-e1427784795864.jpg",
+          bitrh: "",
+        },
+        {
+          id: "3",
+          name: "namae",
+          image_url: "https://nekogazou.com/wp-content/uploads/2015/03/282e6ed4976b181c78381a609c0f4e32-e1427784795864.jpg",
+          bitrh: "",
+        },
+        {
+          id: "3",
+          name: "namae",
+          image_url: "https://nekogazou.com/wp-content/uploads/2015/03/282e6ed4976b181c78381a609c0f4e32-e1427784795864.jpg",
+          bitrh: "",
+        },
+        {
+          id: "3",
+          name: "namae",
+          image_url: "https://nekogazou.com/wp-content/uploads/2015/03/282e6ed4976b181c78381a609c0f4e32-e1427784795864.jpg",
+          bitrh: "",
+        },
+        {
+          id: "3",
+          name: "namae",
+          image_url: "https://nekogazou.com/wp-content/uploads/2015/03/282e6ed4976b181c78381a609c0f4e32-e1427784795864.jpg",
+          bitrh: "",
+        },{
+          id: "3",
+          name: "namae",
+          image_url: "https://nekogazou.com/wp-content/uploads/2015/03/282e6ed4976b181c78381a609c0f4e32-e1427784795864.jpg",
+          bitrh: "",
+        }
+      ]
+    },
+    created() {
+      this.getRooms();
+    },
+    methods: {
+      getRooms() {
+        // fetch('/api/rooms').then(response => response.json()).then(data => {
+        //   this.rooms = data.result;
+        // });
+      }
+    }
+  });
+})();
