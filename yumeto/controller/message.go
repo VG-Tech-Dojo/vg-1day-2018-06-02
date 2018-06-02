deaffd4f55a4d1a7aaf0b2ba78b9151c6c526483
Yumeto Inaoka <in@okayu.jp>
2018-06-02 14:05:10 +0900

    tutorial-1-2-yumeto-fix-typo

--- yumeto/controller/message.go
+++ yumeto/controller/message.go
@@ -76,7 +76,7 @@ func (m *Message) Create(c *gin.Context) {
 	// Tutorial 1-2. ユーザー名を追加しよう
 	// できる人は、ユーザー名が空だったら`anonymous`等適当なユーザー名で投稿するようにしてみよう
 	if msg.Username == "" {
-		msg.Username = "anonymus"
+		msg.Username = "anonymous"
 	}
 
 	inserted, err := msg.Insert(m.DB)
